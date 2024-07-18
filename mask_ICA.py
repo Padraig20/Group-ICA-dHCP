c5edf644e05bb89daea6d3092b3053ad5f6d2cdd
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-07-18 17:41:51 +0900

    add argparse for masking group ICA map

--- mask_ICA.py
+++ mask_ICA.py
@@ -1,14 +1,23 @@
 import nibabel as nb
 import numpy as np
+import argparse
 import nilearn.image 
 import nilearn.masking
 
+parser = argparse.ArgumentParser(description='Masking the group ICA map')
+
+parser.add_argument('--input', type=str, help='Path to the group ICA map created by MELODIC', default='output/melodic_IC.nii.gz')
+parser.add_argument('--mask', type=str, help='Path to the mask nifti file', default='mask.nii.gz')
+parser.add_argument('--output', type=str, help='Path to save the masked group ICA map as npy file', default='dHCP_groupICA_masked.npy')
+
+args = parser.parse_args()
+
 # load the group ICA map
-nifti_orig = nb.load('melodic_IC.nii.gz')
+nifti_orig = nb.load(args.input)
 
 # load the mask file
-mask_file_nifti = nb.load('mask.nii.gz')
-mask_file=mask_file_nifti.get_fdata().astype(int)
+mask_file_nifti = nb.load(args.mask)
+mask_file = mask_file_nifti.get_fdata().astype(int)
 print(f"Shape of mask: {mask_file.shape}")
 
 # preprocess the mask to fit the group ICA map
@@ -16,9 +25,9 @@ target_affine = nifti_orig.affine
 target_shape = nifti_orig.shape[:3]
 mask = nilearn.image.resample_img(mask_file_nifti,
                                   target_affine=target_affine, 
-                                  target_shape=target_shape,interpolation='nearest')
+                                  target_shape=target_shape, interpolation='nearest')
 
-nb.save(mask,'mask_preprocessed.nii.gz')
+nb.save(mask, f'{args.mask[:-7]}_preprocessed.nii.gz')
 
 print(f"Shape of preprocessed mask: {mask.shape, mask.affine}")
 
@@ -26,5 +35,5 @@ print(f"Shape of preprocessed mask: {mask.shape, mask.affine}")
 masked_pixels = nilearn.masking.apply_mask(nifti_orig, mask_img=mask)
 print(f"Shape of masked group ICA map: {masked_pixels.shape}")
 
-np.save('dHCP_groupICA_masked.npy', masked_pixels)
+np.save(args.output, masked_pixels)
 

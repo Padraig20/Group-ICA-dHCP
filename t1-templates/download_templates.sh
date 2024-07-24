79c95017b74fad24bb2526de9e48a5242f901bf5
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-07-24 16:21:53 +0900

    add script to automatically download t1 weighted templates

--- t1-templates/download_templates.sh
+++ t1-templates/download_templates.sh
@@ -2,7 +2,7 @@
 
 base_url="https://gin.g-node.org/BioMedIA/dhcp-volumetric-atlas-groupwise/raw/master/mean"
 
-echo "This script will download the mask files for gestational ages 36 to 44..."
+echo "This script will download the t1 template files for gestational ages 36 to 44..."
 echo "Make sure you are in the folder where you want to download the files!"
 
 # gestational ages range from 36 to 44
@@ -11,14 +11,14 @@ for ga in {36..44}; do
   read user_input
 
   if [[ "$user_input" == "y" ]]; then
-    file_url="${base_url}/ga_${ga}/mask.nii.gz"
+    file_url="${base_url}/ga_${ga}/template_t1.nii.gz"
     
-    wget -O "mask_ga${ga}.nii.gz" "$file_url"
+    wget -O "template_t1_ga${ga}.nii.gz" "$file_url"
     
     if [[ $? -eq 0 ]]; then
-      echo "Downloaded and renamed mask_ga${ga}.nii.gz successfully."
+      echo "Downloaded and renamed template_t1_ga${ga}.nii.gz successfully."
     else
-      echo "Failed to download mask_ga${ga}.nii.gz."
+      echo "Failed to download template_t1_ga${ga}.nii.gz."
     fi
   else
     echo "Skipped download for ga_${ga}."
